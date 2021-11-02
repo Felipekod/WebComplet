c9445dd5144445da0360131f5dd2e2742c8ba77c
Felipekod <55560328+Felipekod@users.noreply.github.com>
2021-11-02 10:51:51 -0400

    React Calculator display

--- react/calculatrice/src/main/Calculator.jsx
+++ react/calculatrice/src/main/Calculator.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import './Calculator.css'
 import Button from '../components/Button'
+import display from '../components/Display'
+import Display from '../components/Display'
 
 export default class Calculator extends Component {
     render() {
         return(
             <div className="calculator">
+                <Display value={100}/>
                 <Button label="AC"/>
                 <Button label="/"/>
                 <Button label="7"/>
