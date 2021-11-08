304942515dd9508aecb5a83c9dc88667c9eebe6a
Felipekod <55560328+Felipekod@users.noreply.github.com>
2021-11-08 09:17:58 -0500

    React CRUD Form

--- react/crud/frontend/src/components/user/UserCrud.jsx
+++ react/crud/frontend/src/components/user/UserCrud.jsx
@@ -39,10 +39,59 @@ export default class UserCrud extends Component {
         return list
     }
 
+    updateField(event){ //Methode pour mettre à jour le champs du form
+        const user = {...this.state.user} //On clone l'utilisateur du state
+        user[event.target.name] = event.target.value // On utiliste le nom de l'input pour chercher la propriété du 'state'
+        this.setState({user}) 
+    }
+
+    renderForm(){
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nom</label>
+                            <input type="text" 
+                                    className="form-control" 
+                                    name="name" 
+                                    value={this.state.user.name}
+                                    onChange={e => this.updateField(e)}
+                                    placeholder="Voulez saisir le nom..."></input>
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-mail</label>
+                            <input type="text" 
+                                    className="form-control"
+                                    name="email"
+                                    value={this.state.user.email}
+                                    onChange={e => this.updateField(e)}
+                                    placeholder="Voulez saisir l'e-mail">
+                            </input>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary"
+                                onClick={e => this.save(e)}>Enregistrer</button>
+                        <button className="btn btn-secondary ml-2"
+                                onClick={e => this.clear(e)}> Annuler</button>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+
     render() {
         return (
             <Main {...headerProps}>
-                Utilisateur
+                {this.renderForm()}
             </Main>
         )
     }
